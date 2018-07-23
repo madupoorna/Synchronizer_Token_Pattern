2a6ea850dc43e7c21ca292d8c3a91ae87d70e8c7
madupoorna <karunadhipathis@gmail.com>
2018-07-23 16:37:13 +0530

    Initial commit

---
+++ src/main/java/com/example/SynchronizerTokenPattern/SynchronizerTokenPatternApplication.java
@@ -0,0 +1,12 @@
+package com.example.SynchronizerTokenPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizerTokenPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizerTokenPatternApplication.class, args);
+	}
+}
